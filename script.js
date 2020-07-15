cf62b75123dbc8794888f554caa781ae4f867742
Mimi Tran <7123mimit@gmail.com>
2020-07-15 16:24:22 +1000

    add operator function

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+const operate = (operator, x, y) => {
+    switch (operator) {
+        case 'add':
+            return add(x, y);
+            break;
+        case 'subtract':
+            return subtract(x, y);
+            break;
+        case 'multiply':
+            return multiply(x, y);
+            break;
+        case 'divide':
+            return divide(x, y);
+            break;
+    }
+}
